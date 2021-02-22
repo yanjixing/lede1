cd90081e22eec2fe0fd7b3ec52dc95d9c13fd5d0
yanjixing <78809118+yanjixing@users.noreply.github.com>
2021-02-22 23:58:13 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,6 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+git clone https://github.com/frainzy1477/luci-app-clash.git package/luci-app-clash
+git clone https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
