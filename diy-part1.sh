d838919fe844c9d7475c85b5f4a3823015953754
yanjixing <78809118+yanjixing@users.noreply.github.com>
2021-03-29 19:48:42 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,6 +16,6 @@
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
-svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
+#svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
 
-git clone https://github.com/Apocalypsor/luci-app-smartdns.git package/luci-app-smartdns
+#git clone https://github.com/Apocalypsor/luci-app-smartdns.git package/luci-app-smartdns
